3897dbd4492bf771c365ea047b5ca0dacca6d620
ryota-09 <n.ryo950911@gmail.com>
2022-02-21 15:07:47 +0900

    (4-2)数値フォーマット修正

--- src/views/EmployeeDetail.vue
+++ src/views/EmployeeDetail.vue
@@ -62,7 +62,7 @@
             </tr>
             <tr>
               <th nowrap>給料</th>
-              <td><span v-html="currentEmployee.salary"></span>円</td>
+              <td>{{ currentEmployee.salary.toLocaleString() }}円</td>
             </tr>
             <tr>
               <th nowrap>特性</th>
